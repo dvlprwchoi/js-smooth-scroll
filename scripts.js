8c51e8fcbe6ab850ca464b20128e95819e27dd52
dvlprwchoi <dvlpr.wchoi@gmail.com>
2021-10-19 15:18:56 -0500

    Add smoothScroll function

--- scripts.js
+++ scripts.js
@@ -1,1 +1,29 @@
 // console.log('hello');
+
+const body = document.body;
+// console.log(body);
+
+const scrollWrap = document.getElementsByClassName('scrollWrap')[0];
+// console.log(scrollWrap);
+
+const height = scrollWrap.getBoundingClientRect().height - 1;
+// console.log(height);
+
+const speed = 0.2;
+
+///////////////////////////////
+/////////
+///////////////////////////////
+let offset = 0;
+body.style.height = Math.floor(height) + 'px';
+
+function smoothScroll() {
+  offset += (window.pageYOffset - offset) * speed;
+
+  let scroll = 'translateY(-' + offset + 'px) translateZ(0)';
+  scrollWrap.style.transform = scroll;
+
+  callScroll = requestAnimationFrame(smoothScroll);
+}
+
+smoothScroll();
